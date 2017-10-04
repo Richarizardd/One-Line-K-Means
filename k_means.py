aba4c8c00dc7ff84dab1b154d62efc00174ab51e
Richard Chen <rchen40@jhu.edu>
2017-10-04 17:59:25 -0400

    Update k_means.py

--- k_means.py
+++ k_means.py
@@ -1,2 +1,1 @@
-import numpy as np
-KMeansFit = lambda X, centers, max_itr, k: [[np.mean(X[np.argmin([np.linalg.norm(X - m_r, axis=1) for m_r in centers], axis=0) == r], axis=0) for r in range(k)] for itr in range(max_itr)]
+import numpy as np; KMeansFit = lambda X, centers, max_itr, k: [[np.mean(X[np.argmin([np.linalg.norm(X - m_r, axis=1) for m_r in centers], axis=0) == r], axis=0) for r in range(k)] for itr in range(max_itr)]
